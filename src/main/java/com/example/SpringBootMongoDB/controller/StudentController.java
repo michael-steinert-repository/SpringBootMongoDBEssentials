047aeb9aa4c8f55693f9b09b42011c7748f560d4
Michael Steinert <Michael.Steinert.94@gmail.com>
2021-08-22 10:30:46 +0200

    Added Sorting and Pagination

--- src/main/java/com/example/SpringBootMongoDB/controller/StudentController.java
+++ src/main/java/com/example/SpringBootMongoDB/controller/StudentController.java
@@ -3,7 +3,10 @@ package com.example.SpringBootMongoDB.controller;
 import com.example.SpringBootMongoDB.entity.Student;
 import com.example.SpringBootMongoDB.service.StudentService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +19,22 @@ public class StudentController {
     private final StudentService studentService;
 
     @GetMapping
-    public List<Student> findAllStudents() {
-        return studentService.findAllStudents();
+    public ResponseEntity<List<Student>> findAllStudents() {
+        return ResponseEntity.ok().body(studentService.findAllStudents());
+    }
+
+    @GetMapping("/{field}")
+    public ResponseEntity<List<Student>> findAllStudentsWithSorting(@PathVariable String field) {
+        return ResponseEntity.ok().body(studentService.findAllStudentsWithSorting(field));
+    }
+
+    @GetMapping("/pagination/{offset}/{pageSize}")
+    public ResponseEntity<Page<Student>> findAllStudentsWithPagination(@PathVariable int offset, @PathVariable int pageSize) {
+        return ResponseEntity.ok().body(studentService.findAllStudentsWithPagination(offset, pageSize));
+    }
+
+    @GetMapping("/pagination-and-sorting/{field}/{offset}/{pageSize}")
+    public ResponseEntity<Page<Student>> findAllStudentsWithSortingAndPagination(@PathVariable String field, @PathVariable int offset, @PathVariable int pageSize) {
+        return ResponseEntity.ok().body(studentService.findAllStudentsWithSortingAndPagination(field, offset, pageSize));
     }
 }
--- src/main/java/com/example/SpringBootMongoDB/service/StudentService.java
+++ src/main/java/com/example/SpringBootMongoDB/service/StudentService.java
@@ -3,6 +3,9 @@ package com.example.SpringBootMongoDB.service;
 import com.example.SpringBootMongoDB.Repository.StudentRepository;
 import com.example.SpringBootMongoDB.entity.Student;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,5 +17,17 @@ public class StudentService {
 
     public List<Student> findAllStudents() {
         return studentRepository.findAll();
+    }
+
+    public List<Student> findAllStudentsWithSorting(String field) {
+        return studentRepository.findAll(Sort.by(Sort.Direction.ASC, field));
+    }
+
+    public Page<Student> findAllStudentsWithPagination(int offset, int pageSize) {
+        return studentRepository.findAll(PageRequest.of(offset, pageSize));
+    }
+
+    public Page<Student> findAllStudentsWithSortingAndPagination(String field, int offset, int pageSize) {
+        return studentRepository.findAll(PageRequest.of(offset, pageSize).withSort(Sort.Direction.ASC, field));
     }
 }
